4b4132565bf275b4177f46ee27f4913f2d899bf2
amirka7777 <amirmgoian0808@gmail.com>
2026-07-01 14:34:20 +0700

    feat: integrate sqlite storage initialization in server main

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -5,13 +5,25 @@ import (
 	"net"
 
 	"github.com/amir/sys-monitor/internal/server"
+	"github.com/amir/sys-monitor/internal/storage"
 	"github.com/amir/sys-monitor/proto"
 	"google.golang.org/grpc"
 )
 
 func main() {
 
-	log.Println("[Сервер] запущено хранилище метрик")
+	log.Println("[Сервер] запущено хранилище метрик!")
+
+	store, err := storage.NewSqliteStorage("storage.db")
+	if err != nil {
+		log.Fatalf("Не удалось запустить базу данных: %v", err)
+	}
+
+	defer func() {
+		if err := store.Close(); err != nil {
+			log.Printf("Не удалось закрыть базу данных: %v", err)
+		}
+	}()
 
 	listener, err := net.Listen("tcp", ":50051")
 	if err != nil {
@@ -20,13 +32,13 @@ func main() {
 	defer listener.Close()
 
 	grpcServer := grpc.NewServer()
-	metricsHandler := server.NewMetricsServer()
+	metricsHandler := server.NewMetricsServer(store)
 
 	proto.RegisterMetricsServiceServer(grpcServer, metricsHandler)
 
 	log.Println("[Сервер] Успешно запущен и слушает порт 50051")
 
-	if err := grpcServer.Serve(listener); err != nil {
+	if err := grpcServer.Serve(listener); err != nil { // бесконечный прием запросов
 		log.Fatalf("Ошибка при работе gRPC сервера: %v", err)
 	}
 
